712544d423b11439355a32a0be3cd6c477393c83
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-07-23 17:52:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let count;
 let randomNumber;
+let wrongGuessNumber;
 
 function resetGame() {
     randomNumber = parseInt(Math.random() * 101);
-    count = 0;
+    wrongGuessNumber = 0;
+    document.querySelector("input").value = " ";
 }
 
 resetGame();
 
 function guessTheNumber() {
     let input = parseInt(document.querySelector("input").value);
-    if(input > randomNumber) {
+    if (input > randomNumber) {
         alert("to Big");
-        count ++ ; 
+        wrongGuessNumber++;
+    } else if (input < randomNumber) {
+        alert("too Small");
+        wrongGuessNumber++;
+    } else {
+        alert("congratulation you have won ! number of wrong gusses" + wrongGuessNumber);
+        resetGame();
     }
-else if(input < randomNumber) {
-    alert("too Small");
-    count ++;
-}
-else {
-    alert("congratulation you have won ! number of wrong gusses" + count);
-    resetGame();
-}
 }
